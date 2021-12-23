b2463ae01eb561653f8d9deaf616a4a23ddf8180
DYK0120 <Doyeunkim120@gmail.com>
2021-12-22 21:37:24 -0800

    default city weather and first column of 6 day weather forecast added

--- index.js
+++ index.js
@@ -10,14 +10,17 @@ currentDay.innerHTML = days[now.getDay()];
 currentHour.innerHTML = ("0"+now.getHours()).slice(-2); //this will add 0 at the beginning. examples: 1pm -> 01pm. 12pm -> 012pm.  Then, it will take the last two digits (01pm -> 01pm. 012pm -> 12pm).
 currentMinute.innerHTML = ("0"+now.getMinutes()).slice(-2);
 
-
 function searchedCityCurrentWeather(event) {
-  event.preventDefault();  
-  let city = document.querySelector("#search-input").value;  
-  city = city.toLowerCase();
+  let city = event;
+  let defaultCityName = document.querySelector("#city-heading-left");
+  defaultCityName.innerHTML = city;
+  let unit = "metric";
+  let apiKey = '77f5bbd678dbc6585fd33ab51e79f061';
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+  axios.get(apiUrl).then(getTemp);
+}
 
-  function getTemp(data){
-  
+function getTemp(data){
   celciusTemperature = Math.round(data.data.main.temp);
   let description = data.data.weather[0].description;
   let icon = document.querySelector(".weather-emoji-today");
@@ -26,32 +29,35 @@ function searchedCityCurrentWeather(event) {
   currentTemp.innerHTML = celciusTemperature;
   let weatherDescription = document.querySelector("#description");
   weatherDescription.innerHTML = description;
-  let cityName = document.querySelector("#city-heading-left");
-  cityName.innerHTML = city;
   let windSpeed = document.querySelector(".windSpeed");
   windSpeed.innerHTML = data.data.wind.speed;
   }
 
+function getWeather(event){
+    event.preventDefault();
+    searchedCity = document.querySelector("#search-input").value;  
+    searchedCity = searchedCity.toLowerCase();
+    let cityName = document.querySelector("#city-heading-left");
+    cityName.innerHTML = searchedCity;
 
-  let unit = "metric";
-  let apiKey = '77f5bbd678dbc6585fd33ab51e79f061';
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
 
+  let key = '77f5bbd678dbc6585fd33ab51e79f061';
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&appid=${key}&units=metric`;
 
-  axios.get(apiUrl).then(getTemp);
-}
+  axios.get(url).then(getTemp);
+  }
 
 let celciusTemperature = null;
 
 let form = document.querySelector("#city-search-form");
-form.addEventListener("submit", searchedCityCurrentWeather);
+form.addEventListener("submit", getWeather);
 
+//Convert temperature units
 function displayFarenheitUnit (event) {
     event.preventDefault();
     let farenheitTemperature = Math.round((celciusTemperature * 9/5) + 32);
     let currentTemperature = document.querySelector("#current-temperature");
     currentTemperature.innerHTML = farenheitTemperature;
-
     fTemperature.classList.add("active");
     cTemperature.classList.remove("active");
 }
@@ -60,11 +66,9 @@ function displayCelciusUnit (event) {
     event.preventDefault();
     let currentTemperature = document.querySelector("#current-temperature");
     currentTemperature.innerHTML = celciusTemperature;
-
     fTemperature.classList.remove("active");
     cTemperature.classList.add("active");
 }
-
 
 let fTemperature = document.querySelector("#farenheitUnit");
 let cTemperature = document.querySelector("#celciusUnit");
@@ -72,3 +76,4 @@ fTemperature.addEventListener("click", displayFarenheitUnit);
 cTemperature.addEventListener("click", displayCelciusUnit);
 
 
+searchedCityCurrentWeather("New York");
